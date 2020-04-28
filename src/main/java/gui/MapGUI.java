fcd4a820ea2ee16df663203d4bd6b90778cd8ce6
abdo <abdelsalem.yabrag@hotmail.com>
2020-04-28 16:23:57 +0200

    Implemented Load Map

--- src/main/java/gui/MapGUI.java
+++ src/main/java/gui/MapGUI.java
@@ -2,22 +2,29 @@ package gui;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import sun.tools.jstat.Alignment;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
 
-import javax.swing.*;
 
 public class MapGUI extends Application {
     private FlowPane mainContainer;
     private Scene scene;
-
+    private ImageView imageView = new ImageView();
+    private Stage primaryStage;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
         initializeMainContainer();
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -27,7 +34,7 @@ public class MapGUI extends Application {
     private void initializeMainContainer() {
         VBox mainContainer = new VBox();
 //        mainContainer.setOrientation(Orientation.VERTICAL);
-        mainContainer.setBackground(new Background(new BackgroundFill(Color.ROSYBROWN, null, null)));
+//        mainContainer.setBackground(new Background(new BackgroundFill(Color.ROSYBROWN, null, null)));
         mainContainer.getChildren().addAll(getMenuBar(), getTopPane(), getBottomPane());
         scene = new Scene(mainContainer, 800, 500);
 
@@ -36,17 +43,27 @@ public class MapGUI extends Application {
     private MenuBar getMenuBar() {
         MenuBar menuBar = new MenuBar();
         Menu menu = new Menu("File");
-        MenuItem loadItem = new MenuItem("Load Map");
+        MenuItem loadMapItem = new MenuItem("Load Map");
+
+        loadMapItem.setOnAction(e -> {
+            System.out.println("Load Map Item clicked");
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Choose Map");
+            File file = fileChooser.showOpenDialog(primaryStage);
+            loadImage(file.getPath());
+        });
+
         MenuItem loadPlaces = new MenuItem("Load Places");
         MenuItem saveItem = new MenuItem("Save");
         MenuItem exitItem = new MenuItem("Exit");
-        menu.getItems().addAll(loadItem, loadPlaces, saveItem, exitItem);
+        menu.getItems().addAll(loadMapItem, loadPlaces, saveItem, exitItem);
         menuBar.getMenus().add(menu);
         return menuBar;
     }
+
     private Pane getTopPane() {
         HBox topContainer = new HBox();
-        topContainer.setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
+//        topContainer.setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
         topContainer.setMinWidth(800);
         topContainer.setPadding(new Insets(15, 12, 15, 12));
         topContainer.setSpacing(10.0);
@@ -54,6 +71,7 @@ public class MapGUI extends Application {
         Button newButton = new Button("New");
 
         VBox radioBox = new VBox();
+        radioBox.setSpacing(10.0);
         ToggleGroup toggleGroup = new ToggleGroup();
         RadioButton namedRadio = new RadioButton("Named");
         namedRadio.setToggleGroup(toggleGroup);
@@ -83,9 +101,44 @@ public class MapGUI extends Application {
 
     private Pane getBottomPane() {
         HBox bottomBox = new HBox();
-        bottomBox.setMinSize(800, 450);
-        bottomBox.setBackground(new Background(new BackgroundFill(Color.DARKGRAY, null, null)));
+        bottomBox.setMinWidth(800);
+//        bottomBox.setBackground(new Background(new BackgroundFill(Color.DARKGRAY, null, null)));
+        bottomBox.getChildren().addAll(imageView, getBottomRightPane());
         return bottomBox;
+    }
+
+
+    private void loadImage(String path) {
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
+            imageView.setImage(new Image(inputStream));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Pane getBottomRightPane() {
+        VBox vbox = new VBox();
+//        vbox.setBackground(new Background(new BackgroundFill(Color.WHEAT, null, null)));
+        vbox.setPadding(new Insets(15, 12, 15, 12));
+        vbox.setSpacing(10);
+        Label label = new Label("Categories");
+
+        ListView<String> catListView = new ListView<>();
+        List<String> categories = new ArrayList<>();
+        categories.add("Bus");
+        categories.add("Underground");
+        categories.add("Train");
+        catListView.setItems(FXCollections.observableArrayList(categories));
+
+        Button hideCatButton = new Button("Hide Category");
+        hideCatButton.setOnAction(e -> {
+            System.out.println(
+                    catListView.getSelectionModel().getSelectedItem()
+            );
+        });
+        vbox.getChildren().addAll(label, catListView, hideCatButton);
+        return vbox;
     }
 
 }
